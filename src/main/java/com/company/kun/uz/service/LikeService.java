17d1baecc0eea4e09f92cb13cce0158a91ea90be
founder778 <97527689+founder778@users.noreply.github.com>
2022-03-14 21:10:29 +0500

    dasdas

--- src/main/java/com/company/kun/uz/service/LikeService.java
+++ src/main/java/com/company/kun/uz/service/LikeService.java
@@ -20,6 +20,8 @@ public class LikeService {
 
     public LikeDto created(LikeDto dto,Integer userId){
         ProfileEntity profile = profileService.getByid(userId);
+        CommentEntity comment = new CommentEntity();
+        CommentEntity comment2 = new CommentEntity();
         LikeEntity likeEntity = new LikeEntity();
         likeEntity.setProfile(profile);
         likeEntity.setStatus(dto.getStatus());
